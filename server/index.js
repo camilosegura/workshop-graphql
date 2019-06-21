4d5b5ad78949ca63e47ba52a50c6db7b9fb2638b
Camilo Segura <camilosegurar@gmail.com>
2019-06-21 13:47:00 -0500

    Resolved port in Heroku

--- server/index.js
+++ server/index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const next = require('next');
+// const graphqlHTTP = require('express-graphql');
+// const schema = require('./api');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
+const port = process.env.NODE_ENV === 'production' ? 80 : 3000;
 
 app.prepare()
   .then(() => {
     const server = express();
 
+    // server.use('/graphql', graphqlHTTP({
+    //   schema,
+    //   graphiql: true,
+    // }));
     server.get('*', (req, res) => handle(req, res));
 
-    server.listen(3000, (err) => {
+    server.listen(port, (err) => {
       if (err) {
         throw err;
       }
