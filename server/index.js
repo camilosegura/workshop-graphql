c5963dda6ed7cad2909aa0b40db5eabfa053ed82
Camilo Segura <camilosegurar@gmail.com>
2019-06-21 13:50:35 -0500

    Changed Heroku port

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const next = require('next');
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
-const port = process.env.NODE_ENV === 'production' ? 80 : 3000;
+const port = process.env.NODE_ENV === 'production' ? process.env.PORT : 3000;
 
 app.prepare()
   .then(() => {
